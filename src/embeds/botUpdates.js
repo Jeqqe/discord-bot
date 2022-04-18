7945a139944048ed4947b2788c33f52a86bade08
Jeqqe <business.salonen@gmail.com>
2022-04-18 21:07:38 +0300

    Automatic deployment
    Whenever main branch receives an update, it will be automatically updated to the server as well.
    
    (Previous update: Role assignment embed should be mostly functional now.)

---
+++ src/embeds/botUpdates.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js')
+
+const embed = (title, description) => new MessageEmbed()
+  .setColor('#0099ff')
+  .setTitle('Developer updates')
+  .setDescription('Update logs are posted automatically everytime new production build is taken into use.')
+  .addFields([{
+    name: title,
+    value: description.join('\n'),
+    inline: true
+  }])
+
+const sendBotUpdateEmbed = (channel, title, description) => {
+  channel.send({
+    embeds: [embed(title, description)]
+  })
+}
+
+module.exports = { embed, sendBotUpdateEmbed }
